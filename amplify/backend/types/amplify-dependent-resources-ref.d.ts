c3bfe3146f714cfb69159c66b88670c6895ce1a3
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2023-10-04 08:50:17 +0100

    added auth to new amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "beingmarvelblog": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "beingmarvelblog5767c27e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
