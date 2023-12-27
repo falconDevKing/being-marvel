ecf49e05df22c7ee7b522e1d6f2560ceca4ab4a8
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2023-12-27 13:51:31 +0100

    updated image config ish

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = {
       "being-marvel-public-assets.s3.eu-west-1.amazonaws.com",
       "https://beingmarvelblogs381553-dev.s3.eu-west-1.amazonaws.com",
       "beingmarvelblogs381553-dev.s3.eu-west-1.amazonaws.com",
+      "https://beingmarvelblogs3112435-prodd.s3.eu-west-1.amazonaws.com",
+      "beingmarvelblogs3112435-prodd.s3.eu-west-1.amazonaws.com",
       "lh3.googleusercontent.com",
     ],
   },
